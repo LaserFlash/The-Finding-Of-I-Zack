30b0f2e015d93a330ff2568fcf58c9da1c3e2dc2
LaserFlash <bryn.t.bennett@gmail.com>
2017-09-19 16:10:58 +1200

    initial framework

---
+++ src/Controller/GameController.java
@@ -0,0 +1,10 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+
+public class GameController implements java.awt.event.ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/View/Drawable.java
@@ -0,0 +1,10 @@
+package View;
+
+import java.awt.*;
+
+/**
+ *
+ */
+public abstract class Drawable {
+    public abstract void draw(Graphics g);
+}
