b29e6d89b9253140f30c3cfde704b38a2701d493
fieldryan <fieldryan@vuw.ac.nz>
2017-09-19 16:04:15 +1200

    created World package and added initial classes

---
+++ src/World/Game.java
@@ -0,0 +1,12 @@
+package World;
+
+/**
+ * Created by fieldryan on 19/09/17.
+ * Stores all the objects within the game world
+ */
+public class Game {
+
+
+
+
+}
---
+++ src/World/Rooms/itemRoom.java
@@ -0,0 +1,8 @@
+package World.Rooms;
+
+/**
+ * Created by fieldryan on 19/09/17.
+ *
+ */
+public class itemRoom implements Room {
+}
