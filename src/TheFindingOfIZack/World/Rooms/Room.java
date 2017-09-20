b246a713bf271d12399bebb531cc7b0bf1bcdbc1
fieldryan <fieldryan@vuw.ac.nz>
2017-09-20 15:16:50 +1200

    added extends to game and room

--- src/TheFindingOfIZack/World/Rooms/Room.java
+++ src/TheFindingOfIZack/World/Rooms/Room.java
@@ -1,10 +1,12 @@
 package TheFindingOfIZack.World.Rooms;
 
+import TheFindingOfIZack.View.Drawable;
+
 /**
  * Created by fieldryan on 19/09/17.
  * Interface for the general definition of a room
  */
-public abstract class Room {
+public abstract class Room extends Drawable{
 
 
 
