54ebc369b3e2f459708262fd26518b279af86b3e
allanbenj1 <benallan@hotmail.co.nz>
2017-09-20 15:11:05 +1200

    Added getLocation method to entity

--- src/TheFindingOfIZack/Entities/Enemy.java
+++ src/TheFindingOfIZack/Entities/Enemy.java
@@ -1,6 +1,7 @@
 package TheFindingOfIZack.Entities;
 import TheFindingOfIZack.Items.Item;
 import javax.xml.stream.Location;
+import java.awt.*;
 import java.util.List;
 
 /**
@@ -10,7 +11,7 @@ public class Enemy extends Entity {
 
     private int damage;
 
-    public Enemy(int health, Location location, int damage) {
+    public Enemy(int health, Point location, int damage) {
         super(health, location);
         this.health = 0;
         this.damage =damage;
