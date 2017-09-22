b038fb9fbc8d4ba9758c2f85a2b1a083cc786383
gordontheo <gordontheo@myvuw.ac.nz>
2017-09-22 12:25:59 +1200

    Change in constructor, removed mobPlayer

--- src/TheFindingOfIZack/Behaviour/MobEnemy.java
+++ src/TheFindingOfIZack/Behaviour/MobEnemy.java
@@ -1,33 +1,42 @@
 package TheFindingOfIZack.Behaviour;
 
-import javax.xml.stream.Location;
+
+import TheFindingOfIZack.Entities.Player;
+
+import java.awt.*;
+
 
 /**
  * Created by gordontheo on 19/09/17.
  */
 public class MobEnemy {
-    Location location;
+    private Point location;
+    private Player player;
+
+    public MobEnemy(Point location, Player player){
+        this.location = location;
+        this.player = player;
+    }
 
     /**
-     * Shifts the mobs location
-     * @param newX
-     * @param newY
-     * @return
+     * Shifts the mob's location
+     * @param newX change in x
+     * @param newY change in y
+     * @return new location
      */
-    public Location move(double newX, double newY){
-        return location;
-    }
+    public Point move(double newX, double newY){return location;}
 
     /**
      * Overridden in each different mob, is what changes movement behaviour/triggers attack behaviour
      */
-    public void step(){
+    private void step(){
     }
 
     /**
      * To be used for different mob fields of view
      */
-    public double distanceToPlayer(MobPlayer player){
-        return 2.0;//Arbitrary double
+    private double distanceToPlayer(MobPlayer player){
+
+        return 0;//temp
     }
 }
