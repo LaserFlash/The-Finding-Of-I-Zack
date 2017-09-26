9deea5803c0243777416c726c3cbdfd48dd442ff
LaserFlash <bryn.t.bennett@gmail.com>
2017-09-26 15:31:27 +1300

    standard windowsize

--- src/TheFindingOfIZack/View/Panels/GamePanel.java
+++ src/TheFindingOfIZack/View/Panels/GamePanel.java
@@ -1,6 +1,7 @@
 package TheFindingOfIZack.View.Panels;
 
 import TheFindingOfIZack.World.Game;
+import com.sun.corba.se.impl.orbutil.graph.Graph;
 
 import java.awt.*;
 import java.awt.event.ActionListener;
@@ -26,10 +27,40 @@ public class GamePanel extends ScreenPanel {
         super.paintComponent(g);
         System.out.println("Trying to draw");
         //TODO get stuff from model to paint
+        /*
+         * Draw player information such as health, armour, items etc
+         */
+        drawHealth(g);
+        drawPlayerItems(g);
+        drawPlayerArmour();
+        drawRoom(g);
         //Health
         //Items
         //Room
         //Enemy
         //Player
+    }
+
+    private void drawHealth(Graphics g){
+
+    }
+
+    private void drawPlayerItems(Graphics g){
+
+    }
+
+    private void drawPlayerArmour(Graphics g){
+
+    }
+
+    private void drawRoom(Graphics g){
+
+    }
+
+    private void drawEntities(Graphics g){
+
+    }
+    private void drawPlayer(Graphics g){
+
     }
 }
