8411183815debfb153596e5a41332f69f51b5122
gordontheo <gordontheo@myvuw.ac.nz>
2017-09-27 15:02:35 +1300

    Basically I restructured all of Behaviour hopefully it will make everything easier in future, added a new class which acts as a constructor thing

--- src/TheFindingOfIZack/Behaviour/MobFast.java
+++ src/TheFindingOfIZack/Behaviour/MobFast.java
@@ -5,11 +5,7 @@ import java.awt.*;
 /**
  * Created by gordontheo on 19/09/17.
  */
-public class MobFast extends MobEnemy {
-    public MobFast(String type) {
-        super(type);
-    }
-
+public class MobFast extends Mob {
     @Override
     public Point step(Point location, Point player){
         //performs Mod specific step
