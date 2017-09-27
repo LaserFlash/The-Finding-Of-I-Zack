2895f6fa757af0e2299d46996215c53de3458b21
fieldryan <fieldryan@vuw.ac.nz>
2017-09-27 14:29:42 +1300

    added javadoc comments

--- src/TheFindingOfIZack/World/Model.java
+++ src/TheFindingOfIZack/World/Model.java
@@ -9,4 +9,5 @@ public interface Model {
     Player getPlayer();
     void startGameLoop();
     void stopGameLoop();
+
 }
---
+++ src/TheFindingOfIZack/World/Rooms/Door.java
@@ -0,0 +1,18 @@
+package TheFindingOfIZack.World.Rooms;
+
+import TheFindingOfIZack.View.Drawable;
+
+import java.awt.*;
+
+/**
+ * Created by fieldryan on 27/09/17.
+ */
+public class Door implements Drawable {
+
+
+    
+    @Override
+    public void draw(Graphics g) {
+
+    }
+}
