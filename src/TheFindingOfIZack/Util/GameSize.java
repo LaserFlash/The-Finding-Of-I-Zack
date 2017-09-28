b4e2e606de6c7816fc2ce6cd0d42dcf1ae3a3258
allanbenj1 <benallan@hotmail.co.nz>
2017-09-29 12:59:00 +1300

    Fixed wall collisions

--- src/TheFindingOfIZack/Util/GameSize.java
+++ src/TheFindingOfIZack/Util/GameSize.java
@@ -4,12 +4,12 @@ public class GameSize {
     public static final int WINDOW_WIDTH    = 900;
     public static final int WINDOW_HEIGHT   = 600;
     public static final int MENU_HEIGHT = 50;
-    public static final int GAME_HEIGHT = 550;
+    public static final int GAME_HEIGHT = WINDOW_HEIGHT-MENU_HEIGHT;
 
     public static final int WALL_WIDTH = 40;
     public static final int LEFT_WALL = WALL_WIDTH;
     public static final int RIGHT_WALL = WINDOW_WIDTH-WALL_WIDTH;
     public static final int TOP_WALL = WALL_WIDTH;
-    public static final int BOTTOM_WALL = WINDOW_HEIGHT+WALL_WIDTH;
+    public static final int BOTTOM_WALL = GAME_HEIGHT-WALL_WIDTH;
 
 }
