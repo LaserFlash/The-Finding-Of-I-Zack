872d541eaa1f5f917d8a4cdc1230217758c78cb1
LaserFlash <bryn.t.bennett@gmail.com>
2017-09-29 15:02:30 +1300

    better save testing

--- src/Tests/FileIO/LoadFileTest.java
+++ src/Tests/FileIO/LoadFileTest.java
@@ -1,9 +1,17 @@
 package Tests.FileIO;
 
+import TheFindingOfIZack.Entities.Player;
 import TheFindingOfIZack.FileIO.GameFile;
 import TheFindingOfIZack.FileIO.LoadFile;
+import TheFindingOfIZack.FileIO.SaveFile;
 import TheFindingOfIZack.FileIO.Util.InvalidFileException;
+import TheFindingOfIZack.Util.GameSize;
+import TheFindingOfIZack.World.Game;
 import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by woodjess3 on 27/09/17.
@@ -16,9 +24,15 @@ public class LoadFileTest {
     @Test
     public void test01_createLoadFile(){
         try {
+            SaveFile saveFile = new SaveFile(new Game(new Player(999,new Point(GameSize.GAME_WIDTH /2 - 20,GameSize.GAME_HEIGHT/2 - 20))));
             LoadFile testLoadFile = new LoadFile();
+            assertTrue("There should be a game",testLoadFile.getGame() != null);
+            assertTrue("There should be a player",testLoadFile.getGame().getPlayer() != null);
+            assertTrue("Player Health should be 999",testLoadFile.getGame().getPlayer().getMaxHealth() == 999 );
         } catch (InvalidFileException e) {
             e.printStackTrace();
         }
+
+
     }
 }
--- src/Tests/FileIO/SaveFileTest.java
+++ src/Tests/FileIO/SaveFileTest.java
@@ -1,8 +1,13 @@
 package Tests.FileIO;
 
+import TheFindingOfIZack.Entities.Player;
 import TheFindingOfIZack.FileIO.SaveFile;
 import TheFindingOfIZack.FileIO.Util.InvalidFileException;
+import TheFindingOfIZack.Util.GameSize;
+import TheFindingOfIZack.World.Game;
 import org.junit.Test;
+
+import java.awt.*;
 
 import static org.junit.Assert.fail;
 
@@ -17,7 +22,8 @@ public class SaveFileTest {
     @Test
     public void test01_createSaveFile(){
         try {
-            SaveFile testSaveFile = new SaveFile(null);
+            SaveFile testSaveFile = new SaveFile(new Game(new Player(100,new Point(GameSize.GAME_WIDTH /2 - 20,GameSize.GAME_HEIGHT/2 - 20))));
+
         } catch (InvalidFileException e) {
             fail(e.getMessage());
         }
