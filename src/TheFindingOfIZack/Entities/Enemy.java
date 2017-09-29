2c5b877b141277afa497902af8ab59366860ae03
gordontheo <gordontheo@myvuw.ac.nz>
2017-09-29 13:21:47 +1300

    Making Entity Enemy cause damage to player at each step if they have collided

--- src/TheFindingOfIZack/Entities/Enemy.java
+++ src/TheFindingOfIZack/Entities/Enemy.java
@@ -33,12 +33,22 @@ public class Enemy extends Entity {
 
     /**
      * Makes each mob run through its step method
+     * Also checks for edge of map collisions
+     * Also checks to see if the mob has touched the player
      */
     public void move() {
-        Point potentialStep = behaviour.step(location, this.world.getPlayer().getLocation());
-        if(canMove(potentialStep)) {
+        Point player = this.world.getPlayer().getLocation();
+        Point potentialStep = behaviour.step(location, player);
+        //potentialStep represents the move which will take place is there is no obstacle,
+        // also checks if mob is currently touching player
+        if(canMove(potentialStep) && !collision(location,player)) {
             this.location = potentialStep;
             this.box = new BoundingBox(potentialStep.getX(), potentialStep.getY(), this.width, this.width);
+        }
+        if(collision(location,player)){
+            this.world.getPlayer().damage(this.damage); //Not sure what this.damage actually is,
+                                                        // hopefully it hurts the player
+                                                        // (assuming mobs are given a damage value at some point)
         }
     }
 
@@ -49,23 +59,24 @@ public class Enemy extends Entity {
      * @return if the point is clear return true
      */
     private boolean canMove(Point location){
-        if(location.getY() < TOP_WALL){
-            return false;
-        }
-        if(location.getY() < LEFT_WALL){
-            return false;
-        }
-        if(location.getY()+width > BOTTOM_WALL){
-            return false;
-        }
-        if(location.getY()+width > RIGHT_WALL){
-            return false;
-        }
+        if(location.getY() < TOP_WALL){return false;}
+        if(location.getY() < LEFT_WALL){return false;}
+        if(location.getY()+width > BOTTOM_WALL){return false;}
+        if(location.getY()+width > RIGHT_WALL){return false;}
         return true;
     }
 
-    public void collisions(Point location) {
-
+    /**
+     * Determines if the player and any mob have collided
+     * @param mob mob location
+     * @param player player location
+     */
+    public boolean collision(Point mob, Point player) {
+        if(mob.getY() < player.getX()+width){return true;}
+        if(mob.getY() < player.getY()+width){return true;}
+        if(mob.getY()+width > player.getX()){return true;}
+        if(mob.getY()+width > player.getY()){return true;}
+        return false;
     }
 
 }
