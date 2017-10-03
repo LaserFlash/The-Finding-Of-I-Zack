45167bdc80c78100f37bf42f42852b7240fb01b0
LaserFlash <bryn.t.bennett@gmail.com>
2017-10-03 16:22:22 +1300

    document tests

--- src/Tests/View/ViewManagerTest.java
+++ src/Tests/View/ViewManagerTest.java
@@ -13,6 +13,9 @@ public class ViewManagerTest {
 
 
     @Test
+    /**
+     * Check that the view correctly displays the start screen menu
+     */
     public void testMenuDisplay() throws InterruptedException {
 
         MockModel m = new MockModel();
@@ -20,6 +23,9 @@ public class ViewManagerTest {
     }
 
     @Test
+    /**
+     * Check the game game be displayed and then returned to the start menu screen
+     */
     public void testGameDisplayToggle() throws InterruptedException {
         MockModel m = new MockModel();
         SwingUtilities.invokeLater(()->{
@@ -32,7 +38,29 @@ public class ViewManagerTest {
         Thread.sleep(4000);
     }
 
+    @Test
+    /**
+     * Check that the initial disabled buttons become enabled
+     * Disabled buttons are:
+     *      Save
+     *      Resume
+     */
+    public void testActivateOtherButtons() throws InterruptedException {
+        MockModel m = new MockModel();
+        SwingUtilities.invokeLater(()->{
+            ViewManager v = new ViewManager(m);
+            v.showGUI();
+            new Timer(2000, e-> v.enableOtherButtons()).start();
+        });
+        Thread.sleep((3000));
+    }
 
+
+    /**
+     * Restrict the window to being displayed for 3 second period
+     * @param mock
+     * @throws InterruptedException
+     */
     void killIn3Sec(Model mock) throws InterruptedException {
         SwingUtilities.invokeLater(()->{
             ViewManager v = new ViewManager(mock);
