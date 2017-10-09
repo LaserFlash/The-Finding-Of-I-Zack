351a974fa1d0b429cc782ab5accc626e165640bb
Ben <benallan@hotmail.co.nz>
2017-10-09 16:55:01 +1300

    Added urns drawing the pot image

--- src/TheFindingOfIZack/Entities/Urn.java
+++ src/TheFindingOfIZack/Entities/Urn.java
@@ -29,28 +29,6 @@ public class Urn extends Entity implements Savable{
 
     @Override
     public void draw(Graphics g) {
-       /* g.setColor(Color.YELLOW);
-
-        Polygon top = new Polygon();
-
-        top.addPoint((int) location.getX(), (int) location.getY());
-        top.addPoint((int) location.getX()+width, (int) location.getY());
-        top.addPoint((int) location.getX()+width/2, (int) location.getY()+width);
-
-        Polygon btm = new Polygon();
-
-        btm.addPoint((int) location.getX(), (int) location.getY()+width);
-        btm.addPoint((int) location.getX()+width, (int) location.getY()+width);
-        btm.addPoint((int) location.getX()+width/2, (int) location.getY());
-
-        g.fillPolygon(top);
-        g.fillPolygon(btm);
-
-        g.setColor(Color.black);
-
-        g.drawPolygon(top);
-        g.drawPolygon(btm);*/
-
         g.drawImage(urns, (int) location.getX(), (int) location.getY(), null);
 
     }
