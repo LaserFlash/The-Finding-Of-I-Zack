0894bd29045fe7c928179584820e454dced7f663
fieldryan <fieldryan@vuw.ac.nz>
2017-10-10 16:08:28 +1300

    draws doors and room player stuff

--- src/TheFindingOfIZack/World/Rooms/Door.java
+++ src/TheFindingOfIZack/World/Rooms/Door.java
@@ -2,14 +2,23 @@ package TheFindingOfIZack.World.Rooms;
 
 import TheFindingOfIZack.FileIO.Util.Savable;
 import TheFindingOfIZack.Util.GameSize;
+import TheFindingOfIZack.Util.ImageLoader;
 import TheFindingOfIZack.View.Drawable;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.io.IOException;
 
 /**
  * Created by fieldryan on 27/09/17.
  */
 public class Door implements Drawable, Savable {
+
+    private static Image openDoorImage;
+    private static Image closedDoorImage;
+    private static Image lockedDoorImage;
 
     private Room entry;
     private Room destination;
@@ -24,6 +33,9 @@ public class Door implements Drawable, Savable {
         this.destination = destination;
         this.position = position;
         this.isLocked = true;
+        initialiseOPenImage();
+        initialiseClosedImage();
+
     }
 
     public Room getDestination(){
@@ -56,24 +68,149 @@ public class Door implements Drawable, Savable {
     public void draw(Graphics g) {
         if(this.isLocked){
             g.setColor(Color.red);
+            if(this.position == 3){
+                //g.fillRect(0,GameSize.GAME_HEIGHT/2 - this.height/2,GameSize.WALL_WIDTH,height);
+                g.drawImage(closedDoorImage,0,GameSize.GAME_HEIGHT/2 - this.height/2,null);
+
+            }
+            if(this.position == 2){
+                g.drawImage(closedDoorImage,GameSize.GAME_WIDTH/2 - height/2,GameSize.GAME_HEIGHT - GameSize.WALL_WIDTH,null);
+               // g.fillRect(GameSize.GAME_WIDTH/2 - height/2,GameSize.GAME_HEIGHT - GameSize.WALL_WIDTH, height,GameSize.WALL_WIDTH);
+            }
+            if(this.position == 0){
+
+                //g.fillRect(GameSize.GAME_WIDTH/2 - height/2,0, height,GameSize.WALL_WIDTH);
+                g.drawImage(closedDoorImage,GameSize.GAME_WIDTH/2 - height/2,0,null);
+            }
+            if(this.position == 1){
+                //g.fillRect(GameSize.GAME_WIDTH - GameSize.WALL_WIDTH,GameSize.GAME_HEIGHT/2 - this.height/2,GameSize.WALL_WIDTH,height);
+                g.drawImage(closedDoorImage,GameSize.GAME_WIDTH - GameSize.WALL_WIDTH,GameSize.GAME_HEIGHT/2 - this.height/2,null);
+            }
         }else {
             g.setColor(Color.green);
-        }
-        if(this.position == 3){
-            g.fillRect(0,GameSize.GAME_HEIGHT/2 - this.height/2,GameSize.WALL_WIDTH,height);
-        }
-        if(this.position == 2){
+            if(this.position == 3){
+                //g.fillRect(0,GameSize.GAME_HEIGHT/2 - this.height/2,GameSize.WALL_WIDTH,height);
+                g.drawImage(openDoorImage,0,GameSize.GAME_HEIGHT/2 - this.height/2,null);
 
-            g.fillRect(GameSize.GAME_WIDTH/2 - height/2,GameSize.GAME_HEIGHT - GameSize.WALL_WIDTH, height,GameSize.WALL_WIDTH);
-        }
-        if(this.position == 0){
+            }
+            if(this.position == 2){
+                g.drawImage(openDoorImage,GameSize.GAME_WIDTH/2 - height/2,GameSize.GAME_HEIGHT - GameSize.WALL_WIDTH,null);
+                // g.fillRect(GameSize.GAME_WIDTH/2 - height/2,GameSize.GAME_HEIGHT - GameSize.WALL_WIDTH, height,GameSize.WALL_WIDTH);
+            }
+            if(this.position == 0){
 
-            g.fillRect(GameSize.GAME_WIDTH/2 - height/2,0, height,GameSize.WALL_WIDTH);
-        }
-        if(this.position == 1){
-            g.fillRect(GameSize.GAME_WIDTH - GameSize.WALL_WIDTH,GameSize.GAME_HEIGHT/2 - this.height/2,GameSize.WALL_WIDTH,height);
+                //g.fillRect(GameSize.GAME_WIDTH/2 - height/2,0, height,GameSize.WALL_WIDTH);
+                g.drawImage(openDoorImage,GameSize.GAME_WIDTH/2 - height/2,0,null);
+            }
+            if(this.position == 1){
+                //g.fillRect(GameSize.GAME_WIDTH - GameSize.WALL_WIDTH,GameSize.GAME_HEIGHT/2 - this.height/2,GameSize.WALL_WIDTH,height);
+                g.drawImage(openDoorImage,GameSize.GAME_WIDTH - GameSize.WALL_WIDTH,GameSize.GAME_HEIGHT/2 - this.height/2,null);
+            }
         }
 
 
+
+    }
+
+
+
+
+    public void initialiseOPenImage() {
+        Image img = new Image() {
+            @Override
+            public int getWidth(ImageObserver observer) {
+                return 0;
+            }
+
+            @Override
+            public int getHeight(ImageObserver observer) {
+                return 0;
+            }
+
+            @Override
+            public ImageProducer getSource() {
+                return null;
+            }
+
+            @Override
+            public Graphics getGraphics() {
+                return null;
+            }
+
+            @Override
+            public Object getProperty(String name, ImageObserver observer) {
+                return null;
+            }
+        };
+
+        try {
+            if(this.position == 0) {
+                img = ImageIO.read(ImageLoader.class.getResource(("/openDoorTop.png")));
+            }
+            if(this.position == 1) {
+                img = ImageIO.read(ImageLoader.class.getResource(("/openDoorRight.png")));
+            }
+            if(this.position == 2) {
+                img = ImageIO.read(ImageLoader.class.getResource(("/openDoorDown.png")));
+            }
+            if(this.position == 3) {
+                img = ImageIO.read(ImageLoader.class.getResource(("/openDoorLeft.png")));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            openDoorImage = img;
+        }
+    }
+
+
+    public void initialiseClosedImage() {
+        Image img = new Image() {
+            @Override
+            public int getWidth(ImageObserver observer) {
+                return 0;
+            }
+
+            @Override
+            public int getHeight(ImageObserver observer) {
+                return 0;
+            }
+
+            @Override
+            public ImageProducer getSource() {
+                return null;
+            }
+
+            @Override
+            public Graphics getGraphics() {
+                return null;
+            }
+
+            @Override
+            public Object getProperty(String name, ImageObserver observer) {
+                return null;
+            }
+        };
+
+        try {
+            if(this.position == 0) {
+                img = ImageIO.read(ImageLoader.class.getResource(("/closedDoorTop.png")));
+            }
+            if(this.position == 1) {
+                img = ImageIO.read(ImageLoader.class.getResource(("/closedDoorRight.png")));
+            }
+            if(this.position == 2) {
+                img = ImageIO.read(ImageLoader.class.getResource(("/closedDoorDown.png")));
+            }
+            if(this.position == 3) {
+                img = ImageIO.read(ImageLoader.class.getResource(("/closedDoorLeft.png")));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            closedDoorImage = img;
+        }
     }
 }
--- src/TheFindingOfIZack/World/Rooms/Room.java
+++ src/TheFindingOfIZack/World/Rooms/Room.java
@@ -2,6 +2,7 @@ package TheFindingOfIZack.World.Rooms;
 
 
 import TheFindingOfIZack.Entities.Player;
+import TheFindingOfIZack.Items.Item;
 import TheFindingOfIZack.Util.GameSize;
 import TheFindingOfIZack.Util.ImageLoader;
 import TheFindingOfIZack.View.Drawable;
@@ -11,6 +12,8 @@ import java.awt.*;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by fieldryan on 19/09/17.
@@ -19,22 +22,42 @@ import java.io.IOException;
 public abstract class Room implements Drawable {
 
     private static Image roomImage;
-
+    private ArrayList<Item> collectables;
     public transient Door northDoor;
     public transient Door eastDoor;
     public transient Door southDoor;
     public transient Door westDoor;
     public boolean isCleared;
+    private Player player;
 
     public Room() {
+        this.collectables = new ArrayList<Item>();
+        this.player = null;
         this.northDoor = null;
         this.eastDoor = null;
         this.southDoor = null;
         this.westDoor = null;
         initialiseImage();
     }
+    public ArrayList<Item> getCollectables(){
+        return this.collectables;
+    }
 
+    public void addPlayer(Player p){
+        this.player = p;
+    }
+
+    public Player getPlayer(){
+        return this.player;
+    }
+
+    public void removePlayer(){
+        this.player = null;
+    }
     public void draw(Graphics g){
+
+
+        g.drawImage(roomImage,0,0,null);
         if(this.westDoor!= null){
             this.westDoor.draw(g);
         }
@@ -47,8 +70,6 @@ public abstract class Room implements Drawable {
         if(this.eastDoor != null){
             this.eastDoor.draw(g);
         }
-
-        g.drawImage(roomImage,0,0,null);
 
     }
 
