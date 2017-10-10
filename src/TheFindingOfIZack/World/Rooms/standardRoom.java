c1f2ef18c8d827842cecdc6ae749fc06f0efb5fb
fieldryan <fieldryan@myvuw.ac.nz>
2017-10-11 00:44:49 +1300

    random number of enemies spawn

--- src/TheFindingOfIZack/World/Rooms/standardRoom.java
+++ src/TheFindingOfIZack/World/Rooms/standardRoom.java
@@ -45,19 +45,20 @@ public class standardRoom extends Room{
         int numRocks = (int) (Math.random() * 5) + 1;
         int numUrns = (int) (Math.random() * 2) ;
         int numEnemies = (int) (Math.random() * 3) + 1 ;
-        for(int i = 0; i < numRocks; i++){
+      for(int i = 0; i < numRocks; i++){
             items.add(new Rock(randomPoint()));
         }
 
         for(int i = 0; i < numUrns; i++){
             items.add(new Urn(randomPoint()));
         }
-
+        enemiesInRoom.add(new Enemy(randomPoint(),p));
+        /**
         for(int i = 0; i < numEnemies; i++){
             enemiesInRoom.add(new Enemy(randomPoint(),p));
         }
 
-
+        **/
 
 
 
