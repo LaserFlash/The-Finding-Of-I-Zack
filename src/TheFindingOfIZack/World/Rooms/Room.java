6b305a8b6f717a738afde0102bb9838100d0b985
Bryn Bennett <bryn.t.bennett@gmail.com>
2017-10-13 19:21:48 +1300

    move duplicate code to abstract room

--- src/TheFindingOfIZack/World/Rooms/Room.java
+++ src/TheFindingOfIZack/World/Rooms/Room.java
@@ -4,6 +4,7 @@ package TheFindingOfIZack.World.Rooms;
 import TheFindingOfIZack.Entities.Enemy;
 import TheFindingOfIZack.Entities.Entity;
 import TheFindingOfIZack.Entities.Player;
+import TheFindingOfIZack.Entities.Point;
 import TheFindingOfIZack.FileIO.Util.Savable;
 import TheFindingOfIZack.Items.Item;
 import TheFindingOfIZack.Util.GameDimensions;
@@ -15,6 +16,7 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by fieldryan on 19/09/17.
@@ -70,14 +72,10 @@ public abstract class Room implements Drawable, Savable {
      * Recreate bounding boxes for things in the rooms
      */
     public void recreateBoundingBoxes(){
-        if (enemiesInRoom != null)
-            enemiesInRoom.forEach(e -> e.setBox());
-        if (deadEnemies != null)
-            deadEnemies.forEach(e -> e.setBox());
-        if (items != null)
-            items.forEach(e -> e.setBox());
-        if (collectibles != null)
-            collectibles.forEach( e -> e.setBox());
+        enemiesInRoom.forEach(e -> e.setBox());
+        deadEnemies.forEach(e -> e.setBox());
+        items.forEach(e -> e.setBox());
+        collectibles.forEach( e -> e.setBox());
     }
     /**
      * adds a player to the room
@@ -236,6 +234,82 @@ public abstract class Room implements Drawable, Savable {
             }
         }
         return false;
+    }
+
+    protected Point randomPoint(){
+        Random r = new Random();
+
+        int left = GameDimensions.LEFT_WALL;  //low x boundary
+        int right = GameDimensions.RIGHT_WALL - Entity.width; //high x boundary, allowing for space after object
+        int top = GameDimensions.TOP_WALL;    //low y boundary
+        int bottom = GameDimensions.BOTTOM_WALL - Entity.width;   //high y boundary, allowing for space after object
+
+        int x = r.nextInt(right - left) + left;
+        int y = r.nextInt(bottom - top) + top;
+
+        boolean suitable = false;
+        int attempts = 0;
+        int MAXATTEMPS = 10000;
+        while (!suitable && attempts < MAXATTEMPS) {
+            //Check if point is blocking a door or potential door
+            //TODO, add to abstract room and door to only check certain door locations
+            if (checkPointBlockingDoor(x,y) && checkPointNotOverlap(x,y)){
+                suitable = true;
+                continue;
+            }
+            x = r.nextInt(right - left) + left;
+            y = r.nextInt(bottom - top) + top;
+            attempts++;
+        }
+        if (attempts >= MAXATTEMPS){
+            return null;
+        }
+        return new Point(x,y);
+    }
+
+    /**
+     * Check if the given point would block a doorway if it belonged to an entity
+     * @param x the x coordinate of point
+     * @param y the y coordinate of point
+     * @return  true if the door is not blocked false otherwise
+     */
+    private boolean checkPointBlockingDoor(int x, int y){
+        if (x >= GameDimensions.GAME_WIDTH/2 - Door.height/2 - Entity.width &&
+                x <= GameDimensions.GAME_WIDTH/2 + Door.height/2){     //potentially blocking north or south door
+            if (y >= GameDimensions.BOTTOM_WALL - Entity.width*2){  //Blocking south door
+                return false;
+            }
+            else if (y <= GameDimensions.TOP_WALL + Entity.width){    //Blocking North door
+                return false;
+            }
+
+        }else if(y >= GameDimensions.GAME_HEIGHT/2 - Door.height/2 - Entity.width &&
+                y <= GameDimensions.GAME_HEIGHT/2 + Door.height/2){     //potentially blocking east or west door
+            if (x <= GameDimensions.LEFT_WALL + Entity.width){
+                return false;
+            }
+            else if(x >= GameDimensions.RIGHT_WALL - Entity.width*2){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check point will not create an entity that overlaps another
+     * @param x x position
+     * @param y y position
+     * @return  true if overlaps with another item or mob
+     */
+    private boolean checkPointNotOverlap(int x, int y) {
+
+        for (Entity i : items){
+            if (i.getBoundingBox().intersects(x,y,x+Entity.width,y+Entity.width)) return false;
+        }
+        for (Entity i : enemiesInRoom){
+            if (i.getBoundingBox().intersects(x,y,x+Entity.width,y+Entity.width)) return false;
+        }
+        return true;
     }
 
 }
