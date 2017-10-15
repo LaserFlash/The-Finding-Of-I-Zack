705389c94c1c6c18e036e8278122634c8e579b55
LaserFlash <bryn.t.bennett@gmail.com>
2017-10-16 12:12:18 +1300

    test inventory panel

--- tests/View/IndividualPanelTests.java
+++ tests/View/IndividualPanelTests.java
@@ -3,7 +3,8 @@ package View;
 import Mocks.MockModel;
 import TheFindingOfIZack.View.Panels.GameArea;
 import TheFindingOfIZack.View.Panels.GameEndScreen;
-import TheFindingOfIZack.World.Game;
+import TheFindingOfIZack.View.Panels.InventoryPanel;
+
 import org.junit.Test;
 
 import javax.swing.*;
@@ -14,42 +15,21 @@ public class IndividualPanelTests {
     public void endScreenDefaultText() throws InterruptedException {
         JFrame f = new JFrame();
         f.add(new GameEndScreen(new String[]{"Game Over"}));
-        f.pack();
-
-        SwingUtilities.invokeLater(()->{
-            f.setVisible(true);
-            new Timer(2000, e-> f.dispose()).start();
-        });
-
-        Thread.sleep((3000));
+        displayTestWindow(f);
     }
 
     @Test
     public void endScreenLongText() throws InterruptedException {
         JFrame f = new JFrame();
         f.add(new GameEndScreen(new String[]{"This is a long one line String, which needs to be made longer and longer and longer"}));
-        f.pack();
-
-        SwingUtilities.invokeLater(()->{
-            f.setVisible(true);
-            new Timer(2000, e-> f.dispose()).start();
-        });
-
-        Thread.sleep((3000));
+        displayTestWindow(f);
     }
 
     @Test
     public void endScreenMultipleLineText() throws InterruptedException {
         JFrame f = new JFrame();
         f.add(new GameEndScreen(new String[]{"This is a multiple line display","With each line in a different","index of the parsed array"}));
-        f.pack();
-
-        SwingUtilities.invokeLater(()->{
-            f.setVisible(true);
-            new Timer(2000, e-> f.dispose()).start();
-        });
-
-        Thread.sleep((3000));
+        displayTestWindow(f);
     }
 
 
@@ -58,11 +38,22 @@ public class IndividualPanelTests {
         GameArea a = new GameArea(new MockModel());
         JFrame f = new JFrame();
         f.add(a);
-        f.pack();
+        displayTestWindow(f);
+    }
 
+    @Test
+    public void drawInventoryPanel() throws InterruptedException {
+        InventoryPanel i = new InventoryPanel(new MockModel());
+        JFrame f = new JFrame();
+        f.add(i);
+        displayTestWindow(f);
+    }
+
+    private void displayTestWindow(JFrame f) throws InterruptedException {
+
+        f.pack();
         SwingUtilities.invokeLater(()->{
             f.setVisible(true);
-            new Timer(1000,e-> a.repaint()).start();
             new Timer(2000, e-> f.dispose()).start();
         });
 
